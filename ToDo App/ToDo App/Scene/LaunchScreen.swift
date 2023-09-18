f32b31404d06d846074e8df8fd431a7de4530615
Zdenko Čepan <zdenocepan@gmail.com>
2023-09-18 13:03:34 +0200

    add launch screen

---
+++ ToDo App/ToDo App/Scene/LaunchScreen.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchScreen.swift
+//  ToDo App
+//
+//  Created by Zdenko Čepan on 18.09.2023.
+//
+
+import SwiftUI
+
+struct LaunchScreen: View {
+    var body: some View {
+        Image("todo-app-logo")
+    }
+}
+
+struct LaunchScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchScreen()
+    }
+}
